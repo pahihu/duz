600378e17bd7085acb34ab5eb4b2ab10b91b7263
Andras Pahi <pahihu@gmail.com>
2025-06-21 08:52:31 +0200

    updated m.bat

--- m.bat
+++ m.bat
@@ -1,4 +1,5 @@
 del/q printer
 del/q teste.tra
-copy /y hello.dck reader
+copy /y cardload.dek+hello.dek reader
+rem copy /y cardload.dek+primes.dek reader
 .\mix.exe -t -u -g 16 -x tester >mix.log 2>&1
